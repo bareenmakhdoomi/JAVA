3f763f38d4cfc672f3161d2a78849f334e1c301c
bareenmakhdoomi <81918142+bareenmakhdoomi@users.noreply.github.com>
2022-07-16 17:32:27 +0530

    Add files via upload

---
+++ JavaApplication8.java
@@ -0,0 +1,30 @@
+
+package javaapplication8;
+
+import java.util.Scanner;
+
+public class JavaApplication8 {
+     public static void main(String[] args) {
+
+   double x , y , z ;
+   Scanner sc = new Scanner(System.in);	
+   System.out.println("Please Enter Three sides of Triangle ");
+   x = sc.nextDouble();
+    y = sc.nextDouble();
+    z = sc.nextDouble();
+    if((x+ y  >  z) && (y + z> x) &&(x + z > y))
+    {
+        if (x == y && y == z )
+        System.out.println("Equilateral Triangle");
+ 
+    else if (x == y || y == z || z == x )
+        System.out.println("Isosceles Triangle");
+ 
+    else
+        System.out.println("Scalene Triangle");
+    }
+    else
+      System.out.println("It is Not a Valid Triangle");
+     }
+    
+}
